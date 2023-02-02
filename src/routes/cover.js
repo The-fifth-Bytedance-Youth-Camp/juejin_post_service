d911553d04dfcda161f16417c0cb6bf99c6e4447
自然醒 <2507983704@qq.com>
2023-02-02 14:44:55 +0800

    feat(uploads): add image uploads to automatically compress with https://tinify.cn

--- src/routes/cover.js
+++ src/routes/cover.js
@@ -1,48 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const sharp = require('sharp');
-const imageSize = require('image-size');
-const multer = require('multer');
-const fs = require('fs');
 const path = require('path');
-const dotenv = require('dotenv');
-dotenv.config();
-const basicURL = `http://localhost:${ process.env.PORT }`;
+const multer = require('multer');
+const tinify = require('tinify');
 
+const upload = multer({ storage: multer.memoryStorage() });
 const uploadsPath = path.join(__dirname, '../../public/uploads');
+const basicURL = `http://localhost:${ process.env.PORT }`;
+tinify.key = process.env.TINIFY_API_KEY;
 
-const storage = multer.diskStorage({
-    destination(req, file, cb) {
-        cb(null, path.join(uploadsPath, './cache'));
-    },
-    filename(req, file, cb) {
-        cb(null, new Date().getTime() + file.originalname);
-    },
-});
-
-router.post('/upload', multer({ storage }).single('cover'), async (req, res) => {
+/**
+ * 上传图片后自动压缩
+ * https://tinify.com
+ */
+router.post('/upload', upload.single('cover'), async (req, res) => {
     const { id } = req.body;
-    const filePath = req.file.path;
     try {
-        let { width } = imageSize(filePath);
-        if (width > 1130) width = 1130;
-        const sharpObj = sharp(filePath);
-        await sharpObj
-            .webp({ quality: 40 })
-            .resize(width)
-            .toFile(path.join(uploadsPath, `./webp/${ id }.webp`));
-        await sharpObj
-            .jpeg({ quality: 40 })
-            .resize(width)
-            .toFile(path.join(uploadsPath, `./jpeg/${ id }.jpeg`));
-        res.send({
-            success: true,
+        const source = tinify.fromBuffer(req.file.buffer);
+        await source.toFile(path.join(uploadsPath, `/jpeg/${ id }.jpeg`));
+        await source.toFile(path.join(uploadsPath, `/webp/${ id }.webp`));
+        res.json({
+            code: 200,
             url: [ `${ basicURL }/webp/${ id }.webp`, `${ basicURL }/jpeg/${ id }.jpeg` ],
         });
-    } catch (err) {
-        res.send({ success: false, err });
-    } finally {
-        if (fs.existsSync(filePath)) fs.unlinkSync(filePath);
+    } catch ({ message }) {
+        res.json({
+            code: 500,
+            msg: message,
+        });
     }
 });
 
