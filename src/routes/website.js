2a1b62f2dccc41b6611e6b5b63ad725ae7ad4597
zdy218 <2744029932@qq.com>
2023-02-05 14:52:03 +0800

    feat(router): add the website watching statistics function

--- src/routes/website.js
+++ src/routes/website.js
@@ -1,17 +1,48 @@
 const express = require('express');
-// const database = require('../utils/database');
+const database = require('../utils/database');
 const router = express.Router();
 
 // 作者文章数量以及排行榜
-router.get('/list/post', (req, res) => {
+router.get('/list/post', async (req, res) => {
     // 记得取消引入注释 database.xxx
     // res.json(req.query); 记得删，不写的话 eslint 报错
-    res.json(req.query);
+    const { name }=req.query;
+    try{
+        const result= await database.select('name','rankList')
+            .from('watch')
+            .where('name',name)
+            .queryList();
+        res.json({
+            code: 200,
+            ...result,
+        });
+    }catch ({ message }) {
+        res.json({
+            code: 500,
+            msg: message,
+        });
+    }
 });
 
 // 网站日全部阅读量
-router.get('/watch/daily', (req, res) => {
-    res.json(req.query);
+router.get('/watch/daily', async (req, res) => {
+
+    const { date }=req.query;
+    try{
+        const result= await database.select('num')
+            .from('watch')
+            .where('date',date)
+            .queryList();
+        res.json({
+            code: 200,
+            ...result,
+        });
+    }catch ({ message }) {
+        res.json({
+            code: 500,
+            msg: message,
+        });
+    }
 });
 
 // 网站月全部阅读量
